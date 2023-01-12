d2b4b8cb1dcef253b4b6183edd0c6c3b52a7e5ed
Zalman Izak <thezalmanian@gmail.com>
2023-01-11 16:08:45 -0800

    Add project files.

---
+++ DieRoller/Die.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DieRoller
+{
+    /// <summary>
+    /// Represents a single six-sided die (1 - 6)
+    /// </summary>
+    public class Die
+    {
+
+    }
+}
---
+++ DieRoller/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace DieRoller
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
