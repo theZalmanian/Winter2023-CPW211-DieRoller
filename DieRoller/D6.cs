3e2d06f93d543f3a83514b06d422079d19d6f672
Zalman Izak <thezalmanian@gmail.com>
2023-01-11 16:13:29 -0800

    Changed class name from "Die" to "D6" - Setup D6 class parameters

---
+++ DieRoller/D6.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DieRoller
+{
+    /// <summary>
+    /// Represents a single six-sided die (1 - 6)
+    /// </summary>
+    public class D6
+    {
+        /// <summary>
+        /// The current face-up value of the d6
+        /// </summary>
+        public byte CurrFaceValue { get; set; }
+
+        /// <summary>
+        /// True if the d6 is currently held; otherwise False
+        /// </summary>
+        public bool IsHeld { get; set; }
+    }
+}
